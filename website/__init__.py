f78a2b93772147707f7ab9abff233879f881f216
Anthony Rodriguez <anthonyrod930@gmail.com>
2025-01-10 15:41:06 -0800

    database is made and works but needs integrating to each endpont

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,6 @@ DB_NAME = "database.db"
 
 
 def create_app():
-
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
@@ -22,26 +21,16 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Journal
 
-
-
-    from .models import User
-
-    with app.app_context():
-        db.create_all()
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.index'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+    create_database(app)
 
     return app  
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+    db_path = path.join(app.root_path, DB_NAME)
+    if not path.exists(db_path):
+        with app.app_context():
+            db.create_all()
+            print('Created Database!')
