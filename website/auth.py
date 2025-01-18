2e7b1b48eaa7e81676f26131370afe0ac5320cde
Anthony Rodriguez <anthonyrod930@gmail.com>
2025-01-17 21:00:08 -0800

    working on combining
    working on functionality but front and back end are togtehr

--- website/auth.py
+++ website/auth.py
@@ -12,32 +12,18 @@ def sign_in():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        key = username
-        # Look up the user in the dictionary
-        if key in users.keys():
-            if users[key]['password'] == password:
-             # Validate the password
-                #check_password_hash(user["password"], password):
-                flash("Logged in successfully!", category="success")
-                #login_user(username) 
-                return render_template(
-        "profile.html",
-        user_logged_in=True,
-        username_placeholder=username,
-        days_kept_up_placeholder=users[username]['days_kept_up'],
-        pet_name_placeholder=users[username]['creature_name'],
-        current_goal_placeholder=users[username]['current_goal'],
-        pet_level_placeholder=users[username]['creature_level'],
-        checked_in_days=users[username]['checked_in_days'],
-        #checked_in_today=checked_in_today,
-    )
-                #return redirect(url_for("views.profile"))
-            else:
-                flash('Incorrect password.', category='danger')
-        else:
-            flash('Username not found.', category='danger')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if user.password == password:
+                flash('Logged in', category='success')
+                return render_template('profile.html')
+    #             return redirect(url_for("views.profile"))
+    #         else:
+    #             flash('Incorrect password.', category='danger')
+    #     else:
+    #         flash('Username not found.', category='danger')
 
-    return render_template('index.html')
+        return render_template('index.html')
 
 @auth.route("logout")
 def logout():
@@ -47,16 +33,20 @@ def logout():
 def registration():
     #data = request.form
     if request.method == 'POST':
-        habit = request.form.get("question1")
+        #habit = request.form.get("question1")
         username = request.form.get("username")
         password = request.form.get("password")
         email = request.form.get("email")
-        character = request.form.get('character')
-        goal = request.form.get("goal-days")
+        #character = request.form.get('character')
+        #goal = request.form.get("goal-days")
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already exists', category='error')
 
         new_user = User(username=username, password=password, email=email,
-                        habit=habit,character=character,experience=0,goal=goal)
-        
+                        habit=0,character=0,experience=0,goal=0)
+        #db.session.commit(new_user)
+        #db.session.commit()
 
 
         # users[name] = {}
@@ -69,6 +59,6 @@ def registration():
         # users[name]['password'] = password
         # users[name]['habit'] = habit
 
-        #print(habit,name,password,email,character) 
+        print(username,password,email) 
         return redirect(url_for('views.index'))
     return render_template("registration.html")
