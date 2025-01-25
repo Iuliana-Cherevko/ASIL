29b60cbd28b90ff03c72b208920dd153df49ec77
Iuliana-Cherevko <yuliana@eusby.com>
2025-01-24 17:34:12 -0800

    fix

--- website/views.py
+++ website/views.py
@@ -25,7 +25,18 @@ users = {
         "checked_in_days": ["November 17, 2024"],
         "current_goal": 60,
         "bad_habit_placeholder": "Smoking",
-    }
+    },
+    "Cara": {
+        "days_kept_up": 1,
+        "companion_name": "Fido",
+        "creature_level": 1,
+        "checked_in_days": ["November 17, 2024"],
+        "current_goal": 30,
+        "Fid": "Smoking",
+        "email": "cara@gmail.com",
+        "bad_habit": "Drinking",
+        "companion": "dog",
+    },
 }
 @views.route('Index')
 def index():
@@ -41,30 +52,45 @@ def index():
                 render_template('profile.html', user = current_user)
                 #return redirect(url_for('profile.html', user = current_user))
             
-    return render_template("index.html", user = current_user)
+    return render_template("index.html", user = current_user, companion="dog")
 
+@views.route('Attributions')
+def attributions():
+    return render_template("attributions.html")
 
-@views.route('Profile', methods = ['GET','POST'])
-def profile():
-    username = "Katy Lio"
+@views.route('About')
+def about_us():
+    return render_template("about-us.html")
+
+@views.route('Contact')
+def contact():
+    return render_template("contact.html")
+
+@views.route('Settings', methods = ['GET','POST'])
+def settings():
+    username = "Cara"
     user_data = users[username] 
 
     today = "November 17, 2024"
     checked_in_today = today in user_data['checked_in_days']
 
     return render_template(
-        "profile.html",
+        "settings.html",
         user_logged_in=True,
-        username_placeholder=username,
-        days_kept_up_placeholder=user_data['days_kept_up'],
-        pet_name_placeholder=user_data['creature_name'],
-        current_goal_placeholder=user_data['current_goal'],
-        pet_level_placeholder=user_data['creature_level'],
-        checked_in_days=user_data['checked_in_days'],
-        checked_in_today=checked_in_today,
-        bad_habit_placeholder=user_data['bad_habit_placeholder']
+        username=username,
+        companion_name=user_data['companion_name'],
+        email=user_data['email'],
+        goal_duration=user_data['current_goal'],
+        companion=user_data['companion'],
+        bad_habit=user_data['bad_habit'],
+
+        # days_kept_up_placeholder=user_data['days_kept_up'],
+        # pet_level_placeholder=user_data['creature_level'],
+        # checked_in_days=user_data['checked_in_days'],
+        # checked_in_today=checked_in_today,
+        # bad_habit_placeholder=user_data['bad_habit_placeholder']
     )
-   #return render_template("profile.html")
+#    return render_template("profile.html")
 
 @views.route('Check-in', methods= ['GET', 'POST'])
 def check_in():
@@ -76,6 +102,4 @@ def check_in():
         #return render_template("checkin.html")
     return render_template('checkin.html')
 
-@views.route('/about-us')
-def about_us():
-    return render_template('about-us.html', user=current_user)
+    
